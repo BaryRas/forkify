32e66dfe312150dd1b2ebbd6770d64e772d26d02
BaryRas <gentimechant73@gmail.com>
2019-10-28 18:10:24 +0100

    feat: search
    - And search model, view and controller
    - Add loading spinner
    - Add search pagination

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,52 @@
 /*jshint esversion: 8 */
 // Global app controller
+import { elements, renderLoader, clearLoader } from './base';
+import Search from './models/Search';
+import * as searchView from './views/searchView';
 
-import num from './test';
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
 
-console.log(`I imported  ny this shit ${num} from test`);
+const controlSearch = async () => {
+    // 1- Get query from view
+    const query = searchView.getInput();
+
+    if (query) {
+        // 2- New search object and add to state
+        state.search = new Search(query);
+
+        // 3- Prepare UI for results
+        searchView.cleanInput();
+        searchView.clearResult();
+        renderLoader(elements.resultsSection);
+
+        // 4- Search for recepies
+        await state.search.getResult();
+
+        // 5- Render results on UI
+        clearLoader();
+        searchView.renderResults(state.search.result);
+
+    }
+};
+
+elements.searchControl.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log("I run mother fucker");
+    controlSearch();
+});
+
+elements.resultButtons.addEventListener("click", e => {
+    const btn = e.target.closest(".btn-inline");
+    
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResult();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
